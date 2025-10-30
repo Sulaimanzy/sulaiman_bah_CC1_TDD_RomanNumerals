23b82cadb8175d3b8b7cca907009171aa7649369
Sulaiman Bah <bah.sulaiman@hotmail.com>
2025-10-30 16:37:03 +0100

    refactor: introduce conversion table, simpliy code for better readability, clarify variable names in toRoman function

--- src/RomanNumerals.js
+++ src/RomanNumerals.js
@@ -1,22 +1,24 @@
 class RomanNumerals {
-  toRoman(number) {
-    let roman = "";
+  toRoman(decimalNumber) {
+    let romanNumeral = "";
+    let remainingValue = decimalNumber;
 
-    if (number >= 9) {
-      roman += "IX";
-      number -= 9;
+    const decimalToRomanMappings = [
+      { decimalValue: 9, romanSymbol: "IX" },
+      { decimalValue: 5, romanSymbol: "V" },
+      { decimalValue: 4, romanSymbol: "IV" },
+      { decimalValue: 1, romanSymbol: "I" },
+    ];
+
+    // Conversion using successive subtraction
+    for (const mapping of decimalToRomanMappings) {
+      while (remainingValue >= mapping.decimalValue) {
+        romanNumeral += mapping.romanSymbol;
+        remainingValue -= mapping.decimalValue;
+      }
     }
 
-    if (number >= 4) {
-      roman += "IV";
-      number -= 4;
-    }
-
-    for (let count = 0; count < number; count++) {
-      roman += "I";
-    }
-
-    return roman;
+    return romanNumeral;
   }
 }
 
