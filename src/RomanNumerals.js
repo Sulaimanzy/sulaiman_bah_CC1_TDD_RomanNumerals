8c6bec1fd039b37c6b39c48257747d2bfb756512
Sulaiman Bah <bah.sulaiman@hotmail.com>
2025-10-30 16:15:36 +0100

    feat: add subtractive logic for (IV,IX)

--- src/RomanNumerals.js
+++ src/RomanNumerals.js
@@ -2,6 +2,16 @@ class RomanNumerals {
   toRoman(number) {
     let roman = "";
 
+    if (number >= 9) {
+      roman += "IX";
+      number -= 9;
+    }
+
+    if (number >= 4) {
+      roman += "IV";
+      number -= 4;
+    }
+
     for (let count = 0; count < number; count++) {
       roman += "I";
     }
